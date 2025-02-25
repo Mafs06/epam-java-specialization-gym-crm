46ddfab1536b0e2a663887958c10876617ceb55d
Maria Cabrales <mafecaja@gmail.com>
2025-02-25 17:42:33 -0500

    Enabled annotation-based configuration to the appication context by creating Config class

---
+++ src/main/java/com/epam/campus/gymcrm/config/Config.java
@@ -0,0 +1,10 @@
+package com.epam.campus.gymcrm.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.epam.campus.gymcrm")
+public class Config {
+
+}
